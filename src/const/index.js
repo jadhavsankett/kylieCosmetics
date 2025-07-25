f6baace936678506445ca53ca786e6569b5167f3
student sanket25 <jadhavsanket5656@gamil.com>
2025-07-25 01:25:48 -0400

    delete files

--- src/const/index.js
+++ src/const/index.js
@@ -31,14 +31,6 @@ const flavorlists = [
   },
 ];
 
-const nutrientLists = [
-  { label: "Potassium", amount: "245mg" },
-  { label: "Calcium", amount: "500mg" },
-  { label: "Vitamin A", amount: "176mcg" },
-  { label: "Vitamin D", amount: "5mcg" },
-  { label: "Iron", amount: "1mg" },
-];
-
 const cards = [
   {
     src: "/video/f1.mp4",
@@ -89,4 +81,4 @@ const cards = [
   },
 ];
 
-export { flavorlists, nutrientLists, cards };
+export { flavorlists, cards };
